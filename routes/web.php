3713a72cd5346eb9d5b82b8749457487a5a1910b
Alexander Foster <alexanderfoster@Alexanders-MacBook-Pro.local>
2020-11-07 23:35:42 -0700

    Making nav layout

--- routes/web.php
+++ routes/web.php
@@ -13,5 +13,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', 'GamesController@index')->name('games.index');
-Route::get('/games/{slug}', 'GamesController@show')->name('games.show');
+Route::get('/', function () {
+    return view('index');
+});
